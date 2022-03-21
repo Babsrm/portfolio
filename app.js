807bc68dd9d88fb0236ed34e3d4263fefda322a0
Babsrm <babsruizm@gmail.com>
2022-03-21 19:02:16 +0100

    add transation and animation to every section. works properly

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const sectBtn = document.querySelectorAll('.control');
 const allSections = document.querySelector('.main-content');
 
 function pageTransitions(){
-	//button click active class. Lo recorremos, escuchamos con un click y seleccionamos para añadirle la clase de botón activo. Una vez hecho, a los demás se les cambia la clase de activo a ninguna
+	//button click active class. Lo recorremos, seleccionamos todas las que tengan la clase activa y la metemos en una variable. se le cambia el botón de activo por ninguno y al objeto que hemos escuchado, le añadimos la clase de activo
 	for(let i = 0; i < sectBtn.length; i++) {
 		sectBtn[i].addEventListener('click', function(){
 			let currentBtn = document.querySelectorAll('.active-btn');
@@ -12,6 +12,27 @@ function pageTransitions(){
 			this.className += ' active-btn';
 		})
 	}
+  //section active class
+  allSections.addEventListener('click',(ev)=>{
+    const id = ev.target.dataset.id;
+    if(id){
+      //remove selected from the other buttons
+      sectBtns.forEach((btn) => {
+        btn.classList.remove('active')
+      })
+      ev.target.classList.add('active')
+
+      //hide other sections
+      sections.forEach((section) =>{
+        section.classList.remove('active')
+      })
+
+      const element = document.getElementById(id);
+      element.classList.add('active');
+    }
+  })
 }
+
+
 
 pageTransitions();
