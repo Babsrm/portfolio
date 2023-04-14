9263218528d209dbf7fe8dc10c7dd683ed025b44
Babsrm <babsruizm@gmail.com>
2023-04-14 19:31:24 +0200

    modified javascript for properly transition

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelector('.main-content');
-
-function pageTransitions(){
-	//button click active class. Lo recorremos, seleccionamos todas las que tengan la clase activa y la metemos en una variable. se le cambia el botón de activo por ninguno y al objeto que hemos escuchado, le añadimos la clase de activo
-	for(let i = 0; i < sectBtn.length; i++) {
-		sectBtn[i].addEventListener('click', function(){
-			let currentBtn = document.querySelectorAll('.active-btn');
-			currentBtn[0].className = currentBtn[0].className.replace('active-btn','');
-			this.className += ' active-btn';
-		})
-	}
-  //section active class
-  allSections.addEventListener('click',(ev)=>{
-    const id = ev.target.dataset.id;
-    if(id){
-      //remove selected from the other buttons
-      sectBtns.forEach((btn) => {
-        btn.classList.remove('active')
+(function () {
+  [...document.querySelectorAll(".control")].forEach(button => {
+      button.addEventListener("click", function() {
+          document.querySelector(".active-btn").classList.remove("active-btn");
+          this.classList.add("active-btn");
+          document.querySelector(".active").classList.remove("active");
+          document.getElementById(button.dataset.id).classList.add("active");
       })
-      ev.target.classList.add('active')
-
-      //hide other sections
-      sections.forEach((section) =>{
-        section.classList.remove('active')
-      })
-
-      const element = document.getElementById(id);
-      element.classList.add('active');
-    }
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+      document.body.classList.toggle("light-mode");
   })
-}
+})();
+
 
 
 
